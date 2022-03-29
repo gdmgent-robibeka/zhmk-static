34f63f2829362aee9e7b12f780f3e7541db212f6
gdmgent-robibeka <robin.bekaert2@student.arteveldehs.be>
2022-03-29 14:05:58 +0200

    Workshops carousel + further responsiveness

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,63 @@
-new Glider(document.querySelector('.glider'), {
+new Glider(document.getElementById('glider-workshops'), {
   slidesToShow: 1,
   draggable: true,
-  dots: '#dots',
+  dots: '#dots-workshops',
   responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 5
-      }
-    }
-  ]
+        slidesToShow: 3,
+      },
+    },
+    // {
+    //   breakpoint: 1201,
+    //   settings: {
+    //     slidesToShow: 4,
+    //   },
+    // },
+    // {
+    //   breakpoint: 1501,
+    //   settings: {
+    //     slidesToShow: 5,
+    //   },
+    // },
+  ],
+});
+
+new Glider(document.getElementById('glider-team'), {
+  slidesToShow: 1,
+  draggable: true,
+  dots: '#dots-team',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 1201,
+      settings: {
+        slidesToShow: 4,
+      },
+    },
+    {
+      breakpoint: 1501,
+      settings: {
+        slidesToShow: 5,
+      },
+    },
+  ],
 });
