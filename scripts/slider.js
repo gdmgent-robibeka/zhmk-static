9a1bc675cbb769fd3a42e3997a4d186b56abee4e
gdmgent-robibeka <robin.bekaert2@student.arteveldehs.be>
2022-03-30 12:16:37 +0200

    Fixes

--- scripts/slider.js
+++ scripts/slider.js
@@ -67,13 +67,3 @@ new Glider(document.getElementById('glider-team'), {
     },
   ],
 });
-
-/**
- * Toggle Navigation Hamburger
- */
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('nav');
-
-hamburger.addEventListener('click', () => {
-  nav.classList.toggle('toggled');
-})
