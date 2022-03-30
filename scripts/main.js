3775477b3b9f1dddabbd986c412df99b6d660639
gdmgent-robibeka <robin.bekaert2@student.arteveldehs.be>
2022-03-30 03:02:21 +0200

    Responsiveness homepage, header & footer

--- scripts/main.js
+++ scripts/main.js
@@ -44,7 +44,7 @@ new Glider(document.getElementById('glider-team'), {
     {
       breakpoint: 768,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 3,
       },
     },
     {
@@ -67,3 +67,14 @@ new Glider(document.getElementById('glider-team'), {
     },
   ],
 });
+
+/**
+ * Toggle Navigation Hamburger
+ */
+
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('toggled');
+})
